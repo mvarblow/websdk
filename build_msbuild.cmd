7239f3c3c0e6e6ccfe9652783e6b8395169aa7b9
Matt Mitchell <mmitche@microsoft.com>
2018-04-13 15:13:43 -0700

    Revert "Add logging"
    This reverts commit 57ba7831581dd3f3c230266c027ff42330c39233.

--- build_msbuild.cmd
+++ build_msbuild.cmd
@@ -4,4 +4,4 @@ call "%~dp0\build\EnsureWebSdkEnv.cmd"
 xcopy \\aspnetci\share\tools\websdk\WebDeploy\* "%WebSdkBuild%\WebDeploy\*" /y /C /e /s /f
 
 msbuild "%WebSdkBuild%\BuildPackages.csproj" /t:Restore
-msbuild "%WebSdkBuild%\build.proj" /p:Configuration=%BuildConfiguration% /t:Build %* /v:detailed
+msbuild "%WebSdkBuild%\build.proj" /p:Configuration=%BuildConfiguration% /t:Build %*
