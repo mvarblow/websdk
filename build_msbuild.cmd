21f552ca1a9b175a9bde6866702819d08628fcf0
Matt Mitchell <mmitche@microsoft.com>
2018-04-16 10:20:08 -0700

    More logging

--- build_msbuild.cmd
+++ build_msbuild.cmd
@@ -4,5 +4,5 @@ call "%~dp0\build\EnsureWebSdkEnv.cmd"
 xcopy \\aspnetci\share\tools\websdk\WebDeploy\* "%WebSdkBuild%\WebDeploy\*" /y /C /e /s /f
 
 msbuild "%WebSdkBuild%\BuildPackages.csproj" /t:Restore
-msbuild "%WebSdkBuild%\build.proj" /p:Configuration=%BuildConfiguration% /t:Build %*
-msbuild "%WebSdkBuild%\build.proj" /p:Configuration=%BuildConfiguration% /t:SignPackages %*
+msbuild "%WebSdkBuild%\build.proj" /p:Configuration=%BuildConfiguration% /t:Build %* /v:detailed
+msbuild "%WebSdkBuild%\build.proj" /p:Configuration=%BuildConfiguration% /t:SignPackages %* /v:detailed
